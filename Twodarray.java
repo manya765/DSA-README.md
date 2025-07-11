2d2bf3d2c2f1597b8b7301afd2d2e72255f81cb9
Manya <singhalmanya234@gmail.com>
2025-07-11 19:21:20 +0530

    changes

---
+++ Twodarray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Twodarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // initialising the 2d array
+        int[][] arr = new int[3][3];
+        for(int i=0; i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+         for(int i=0;i<arr.length;i++){
+             for(int j =0;j<arr[i].length;j++){
+                System.out.println(arr[i][j]);
+             }
+             System.out.println( " ");
+
+         }
+         // improved version of array
+         for(int[] a:arr){
+            System.out.println(Arrays.toString(a));
+         }
+    }
+}
