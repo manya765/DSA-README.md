ed3bcfca0c454fc221f8ea2cb81929e0b7114b11
Manya <singhalmanya234@gmail.com>
2025-07-25 23:40:11 +0530

    changes

---
+++ Encode.java
@@ -0,0 +1,17 @@
+import java.nio.charset.StandardCharsets;
+
+public class Encode {
+    public static void main(String[] args) {
+        // encoding a string in java 
+        // function Base64
+        // UTF-8 Encoding valid for characters as well as special characters like !~&*...etc
+        String s="Hello:";
+        //Encode method in UTF8
+        byte[] enc=s.getBytes(StandardCharsets.UTF_8);
+        System.out.println("UTF-8 Bytes"+java.util.Arrays.toString(enc));
+
+        // Decode
+        String decoded= new String(enc,StandardCharsets.UTF_8);
+        System.out.println("Decoded String "+decoded);
+    }
+}
