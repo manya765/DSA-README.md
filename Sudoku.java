bbbe89d7ada912810f225f431e0c4b1bc2ad11b1
Manya <singhalmanya234@gmail.com>
2025-07-30 23:34:00 +0530

    changes

--- Sudoku.java
+++ Sudoku.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class Sudoku {
     public static void main(String[] args) {
         char[][] ch= new char[9][9];
+        // void' type not allowed here
         
 
 
