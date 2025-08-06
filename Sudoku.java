98116d1c7649bea64c0b279b767ae387e4093dea
Manya <singhalmanya234@gmail.com>
2025-08-06 19:12:39 +0530

    changes

--- Sudoku.java
+++ Sudoku.java
@@ -1,8 +1,7 @@
 public class Sudoku {
     public static void main(String[] args) {
         char[][] ch= new char[9][9];
-     //   The method name must exactly match the entity field (userName, not username).
-
+    
 
 
 
