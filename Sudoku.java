e7a87f1f0b29eac6c13a81900df65434b5e3c6b8
Manya <singhalmanya234@gmail.com>
2025-08-16 22:27:18 +0530

    changes

--- Sudoku.java
+++ Sudoku.java
@@ -1,8 +1,8 @@
 public class Sudoku {
     public static void main(String[] args) {
         char[][] ch= new char[9][9];
-          // Sudoku where 36 boxes are divided into 9 portions in which 1 to 9 digits should be unique
           
+
 
 
 
